3131ba69495548ecd2f089d63b3ded3c233a63a5
John Windsor <john.windsor@ucdconnect.ie>
2016-03-10 10:46:50 +0000

    Addition of variable minutes between polling.

--- DublinBikes.py
+++ DublinBikes.py
@@ -14,8 +14,12 @@ import pandas as pd
 NAME="Dublin" # name of contract
 STATIONS="https://api.jcdecaux.com/vls/v1/stations" # and the JCDecaux endpoint
 APIKEY = "731c17d62f2813202e7be680d1447737c3ee0601"
-
+CSV_DIR = "C:\\Users\\winds\\Documents\\UCD Semester 02\\COMP30670 - Software Engineering\\Dublin Bikes\\DublinBikes\\CSV Files\\"
 iCtr = 0
+iMinutes = 0
+
+print(">>>")
+iMinutes = input("Please enter a number for the minutes between polling: ")
 
 while (True):
     strCSV = (strftime("%Y%m%d-%H%M") + ".csv")
@@ -25,7 +29,7 @@ while (True):
         r = requests.get(STATIONS, params={"apiKey": APIKEY, "contract": NAME})
         json_data = json.loads(r.text)
         df = pd.read_json(r.text)
-        df.to_csv(strCSV)
-        time.sleep(300)
+        df.to_csv((CSV_DIR + strCSV))
+        time.sleep(60)
     except:
         print("Error...")
